1bbfaf66cf2a481dbae0eb3f48193a97d7a022bf
Bryce Graham <physeo@hotmail.com>
2020-12-20 23:41:29 -0700

    stuff

--- Program.cs
+++ Program.cs
@@ -6,12 +6,34 @@ namespace Stars
     {
         static void Main(string[] args)
         {
-            int numberOfLoops = 100;
-            StarStuff s = new StarStuff();
-            s.BruteStars();
-            s.LoopStars(numberOfLoops);
-            s.InvertStars(numberOfLoops);
-            s.AlternateStars(numberOfLoops);
+            //Console.WriteLine("1: *");
+            //Console.WriteLine("2: **");
+            //Console.WriteLine("3: ***");
+            //Console.WriteLine("4: ****");
+            //Console.WriteLine("5: *****");
+            //Console.WriteLine("6: ******");
+            //Console.WriteLine("7: *******");
+            //Console.WriteLine("8: ********");
+            //Console.WriteLine("9: *********");
+            //Console.WriteLine("10: **********");
+
+            string myStars = "";
+            int numberOfLoops = 10000;
+            bool invert = false;
+            for (int i = 0; i < numberOfLoops; i++)
+            {
+                if (i > 0 && i % 10 == 0)
+                    invert = !invert;
+
+
+                if (!invert)
+                    myStars += "*";
+                else
+                    myStars = myStars.RemoveEndCharacter();
+
+                string line = i + ": " + myStars;
+                Console.WriteLine(line);
+            }
         }
     }
 }
---
+++ StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stars
+{
+    public static class StringExtension
+    {
+        public static string RemoveEndCharacter(this String s)
+        {
+            return s.Substring(0, s.Length - 1);
+        }
+    }
+}
